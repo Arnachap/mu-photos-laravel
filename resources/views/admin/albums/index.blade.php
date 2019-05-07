ed760fd86b5da6c7825511dea8fedab2de6c30d9
arnachap <arnaud.chapeleur@gmail.com>
2019-05-07 16:13:58 +0200

    Admin album styling

--- resources/views/admin/albums/index.blade.php
+++ resources/views/admin/albums/index.blade.php
@@ -7,47 +7,54 @@
     </button>
 
     @foreach($categories as $category)
-        <table class="table table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col" colspan="2">{{ $category->title }}</th>
-                </tr>
-            </thead>
+        <div class="row mb-1">
+            <div class="col-12">
+                <a class="btn btn-light btn-block text-left" data-toggle="collapse" href="#collapse{{ $category->id }}" role="button" aria-expanded="false" aria-controls="collapse{{ $category->id }}">
+                        {{ $category->title }}
+                </a>
+            </div>
             
-            <tbody>
-                @foreach ($albums as $album)
-                    @if ($album->category == $category->name)
-                        <tr>
-                            <th scope="row">
-                                <a href="/photos/{{ $album->id }}" class="text-dark">{{ $album->title }}</a>
-                            </th>
-                            <td class="text-right">
-                                <a href="/albums/{{ $album->id }}/edit">
-                                    <i class="far fa-edit text-primary"></i>
-                                </a>
+            <div class="collapse col-12" id="collapse{{ $category->id }}">
+                <div class="card card-body">
+                    <table class="table table-borderless table-hover">
+                        <tbody>
+                            @foreach ($albums as $album)
+                                @if ($album->category == $category->name)
+                                    <tr>
+                                        <th scope="row">
+                                            <a href="/photos/{{ $album->id }}" class="text-dark">{{ $album->title }}</a>
+                                        </th>
 
-                                <button type="button" class="btn btn-link pt-0" style="border: none" data-toggle="modal" data-target="#modal{{$album->id}}">
-                                    <i class="fas fa-trash-alt text-danger"></i>
-                                </button>
-
-                                <div class="modal fade" id="modal{{$album->id}}" tabindex="-1" role="dialog" aria-labelledby="modal{{$album->id}}Label" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-body">
-                                                {{ Form::open(['action' => ['AlbumsController@destroy', $album->id], 'method' => 'POST']) }}
-                                                    <p class="text-center">Etes-vous sur de vouloir supprimer l'album {{ $album->title }} ?</p>
-                                                    {{ Form::hidden('_method', 'DELETE') }}
-                                                    {{ Form::submit('Supprimer', ['class' => 'btn btn-danger d-block mx-auto']) }}
-                                                {{ Form::close() }}
+                                        <td class="text-right">
+                                            <a href="/albums/{{ $album->id }}/edit">
+                                                <i class="far fa-edit text-primary"></i>
+                                            </a>
+            
+                                            <button type="button" class="btn btn-link pt-0" style="border: none" data-toggle="modal" data-target="#modal{{$album->id}}">
+                                                <i class="fas fa-trash-alt text-danger"></i>
+                                            </button>
+            
+                                            <div class="modal fade" id="modal{{$album->id}}" tabindex="-1" role="dialog" aria-labelledby="modal{{$album->id}}Label" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-body">
+                                                            {{ Form::open(['action' => ['AlbumsController@destroy', $album->id], 'method' => 'POST']) }}
+                                                                <p class="text-center">Etes-vous sur de vouloir supprimer l'album {{ $album->title }} ?</p>
+                                                                {{ Form::hidden('_method', 'DELETE') }}
+                                                                {{ Form::submit('Supprimer', ['class' => 'btn btn-danger d-block mx-auto']) }}
+                                                            {{ Form::close() }}
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-        </table>
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     @endforeach
 @endsection
