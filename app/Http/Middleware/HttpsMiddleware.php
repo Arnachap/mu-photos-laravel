8ee5709575590df7e6dd4de5273e87ec6ff09f70
arnachap <arnaud.chapeleur@gmail.com>
2019-08-28 11:24:00 +0200

    HTTPS middleware

---
+++ app/Http/Middleware/HttpsMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Middleware;
+use Auth;
+use Closure;
+use App\User;
+class HttpsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
