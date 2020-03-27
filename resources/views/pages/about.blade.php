e9ebbe4fb47df3d11988fc78c5b7cf019bdc93c6
arnachap <arnaud.chapeleur@gmail.com>
2020-03-27 15:57:43 +0100

    Restyling about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,37 +6,31 @@
 
 @section('content')
     <section id="about">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <img class="img-fluid" src="/img/about/about.jpg" alt="Mu' Photos Photographe Nancy">
-                </div>
+        <img class="img-fluid" src="/img/about/about.jpg" alt="Mu' Photos Photographe Nancy">
 
-                <div class="col-md-6">
-                    <h2 class="section-title">Muriel</h2>
-
-                    <h3>25 ans, gymnaste passionnée<br>et passionnée de photographie. </h3>
-
-                    <p>J'ai toujours aimé les photos, pour garder un souvenir des instants
-                        passés. Pour pouvoir
-                        ouvrir
-                        tous ensemble, ou seul dans son canapé, ou autour de notre famille, l'album qui regroupe tous
-                        nos souvenirs de vie.</p>
-
-                    <p>Bref, j'aime les photos ! Alors en grandissant, j'ai appris à les faire
-                        par moi-même. La passion
-                        a grandit encore un peu plus ... Et j'ai décidé de faire le premier pas.</p>
-
-                    <p>Ainsi je suis devenue photographe professionnelle en 2014, tout d'abord
-                        pour la gymnastique !
-                        Depuis ce jour, j'ai diversifié mon activité. Désormais je vous propose des séances variées pour
-                        répondre à vos envies partager ensemble la joie et l'amour qui anime vos familles.</p>
-                </div>
-            </div>
+        <div class="about-text">
+            <h2 class="section-title">Muriel</h2>
+    
+            <h3>25 ans, gymnaste passionnée<br>et passionnée de photographie. </h3>
+    
+            <p>J'ai toujours aimé les photos, pour garder un souvenir des instants
+                passés. Pour pouvoir
+                ouvrir
+                tous ensemble, ou seul dans son canapé, ou autour de notre famille, l'album qui regroupe tous
+                nos souvenirs de vie.</p>
+    
+            <p>Bref, j'aime les photos ! Alors en grandissant, j'ai appris à les faire
+                par moi-même. La passion
+                a grandit encore un peu plus ... Et j'ai décidé de faire le premier pas.</p>
+    
+            <p>Ainsi je suis devenue photographe professionnelle en 2014, tout d'abord
+                pour la gymnastique !
+                Depuis ce jour, j'ai diversifié mon activité. Désormais je vous propose des séances variées pour
+                répondre à vos envies partager ensemble la joie et l'amour qui anime vos familles.</p>
         </div>
     </section>
 
-    <section id="testimonials">
+    {{-- <section id="testimonials">
         <div class="container">
             <h2 class="section-title">Témoignages</h2>
 
@@ -74,5 +68,5 @@
                 </a>
             </div>
         </div>
-    </section>
+    </section> --}}
 @endsection
